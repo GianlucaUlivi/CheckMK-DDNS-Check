985d449b16ea7d7ff07079ccc279014ab39d389a
Gianluca Ulivi <Gianluca.Ulivi@elmec.it>
2023-09-18 10:34:26 +0200

    First version

---
+++ ddns_check.py
@@ -0,0 +1,30 @@
+import requests
+import dns.resolver
+import argparse
+
+
+def main(hostname: str, nameserver: str) -> str:
+    # Get current IP from host connection
+    current_ip = requests.get("https://ifconfig.me/").text
+
+    # Initialize and configure DNS Resolver
+    my_resolver = dns.resolver.Resolver(configure=False)
+    my_resolver.nameservers = [nameserver]
+
+    # Query DNS Resolver for specified hostname
+    dns_answer = my_resolver.resolve(hostname)
+
+    # Verify that DNS Result and Current IP matches
+    if dns_answer[0].to_text() == current_ip:
+        return f"0; DDNS result matches current IP Address ({dns_answer[0].to_text()})"
+    else:
+        return f"1; Current IP: {current_ip}, DDNS Result: {dns_answer[0].to_text()}"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hostname")
+    parser.add_argument("nameserver")
+    args = parser.parse_args()
+
+    print(main(args.hostname, args.nameserver))
